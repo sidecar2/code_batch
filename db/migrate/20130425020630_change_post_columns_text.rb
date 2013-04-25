dd446d2bd746c0e505c7123b824ec7901aaec69d
Andrew Seidler <seidler.andrew@gmail.com>
2013-04-25 09:11:10 -0700

    less errors

--- db/migrate/20130425020630_change_post_columns_text.rb
+++ db/migrate/20130425020630_change_post_columns_text.rb
@@ -2,7 +2,12 @@ class ChangePostColumnsText < ActiveRecord::Migration
   def up
   	change_column :posts, :javascript, :text
   	change_column :posts, :css, :text
-
+  	change_column :posts, :html, :text
+  	change_column :posts, :haml, :text
+  	change_column :posts, :scss, :text
+  	change_column :posts, :javascript, :text
+  	change_column :posts, :php, :text
+  	change_column :posts, :ruby, :text
   end
 
   def down
