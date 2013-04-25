68bd3a5b66aa59cace116281c6426b5da88bbab0
Andrew Seidler <seidler.andrew@gmail.com>
2013-04-25 15:47:25 -0700

    post_errors

--- db/migrate/20130425020630_change_post_columns_text.rb
+++ db/migrate/20130425020630_change_post_columns_text.rb
@@ -1,17 +1,18 @@
 class ChangePostColumnsText < ActiveRecord::Migration
   def up
-  	change_column :posts, :javascript, :text
-  	change_column :posts, :css, :text
-  	change_column :posts, :html, :text
-  	change_column :posts, :haml, :text
-  	change_column :posts, :scss, :text
-  	change_column :posts, :javascript, :text
-  	change_column :posts, :php, :text
-  	change_column :posts, :ruby, :text
+  	change_column :posts, :javascript, :text, :limit => nil
+  	change_column :posts, :css, :text, :limit => nil
+  	change_column :posts, :html, :text, :limit => nil
+  	change_column :posts, :haml, :text, :limit => nil
+  	change_column :posts, :scss, :text, :limit => nil
+  	change_column :posts, :javascript, :text, :limit => nil
+  	change_column :posts, :php, :text, :limit => nil
+  	change_column :posts, :ruby, :text, :limit => nil
+  	change_column :posts, :coffee, :text, :limit => nil
   end
 
   def down
-  	  	change_column :posts, :javascript, :string
+  	  	change_column :posts, :javascript, :string 
   	change_column :posts, :css, :string
   	change_column :posts, :html, :string
   	change_column :posts, :haml, :string
